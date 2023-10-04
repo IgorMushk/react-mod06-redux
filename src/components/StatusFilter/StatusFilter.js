9ae326f2e03b906c3267d0e432bd2289ac2dd9c9
IgorMushk <igor.mushketik@gmail.com>
2023-10-04 18:53:58 +0300

    Subscribe to the store -  TaskList StatusFilter  TaskCounter.
    На данный момент в приложении реализована инициализация стора с инструментами разработчика и подписка компонентов на стор. Следующим шагом будет добавление отправки экшенов.
    
    https://www.edu.goit.global/ru/learn/10292928/31183/31257/textbook

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,21 @@
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css'
+// Импортируем объект значений фильтра
+import { statusFilters } from 'redux/constants';
+// Импортируем хук
+import { useSelector } from 'react-redux';
+import { getStatusFilter } from 'redux/selectors';
 
 export const StatusFilter = () => {
+  // Получаем значение фильтра из состояния Redux
+  //const filter = useSelector(state => state.filters.status);
+  const filter = useSelector(getStatusFilter);
+
     return (
       <div className={css.wrapper}>
-        <Button>All</Button>
-        <Button>Active</Button>
-        <Button>Completed</Button>
+        <Button selected={filter === statusFilters.all}>All</Button>
+        <Button selected={filter === statusFilters.active}>Active</Button>
+        <Button selected={filter === statusFilters.completed}>Completed</Button>
       </div>
     );
   };
