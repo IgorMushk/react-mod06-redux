e50a1ee5e6f40540d8501953ed8f05f272feb435
IgorMushk <igor.mushketik@gmail.com>
2023-10-04 18:04:33 +0300

    Redux DevTools + devToolsEnhancer + enhancer
    npm instalnpm install @redux-devtools/extensionl @redux-devtools/extension
    
    const enhancer = devToolsEnhancer();
    export const store = createStore(rootReducer, enhancer);

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-
 import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 
 // Начальное значение состояния Redux для корневого редюсера,
 // если не передать параметр preloadedState.
@@ -24,4 +24,10 @@ const initialState = {
   //>>>
   console.log(rootReducer());
 
-  export const store = createStore(rootReducer);
+  
+// Создаем расширение стора чтобы добавить инструменты разработчика
+const enhancer = devToolsEnhancer();
+
+
+ //export const store = createStore(rootReducer);
+ export const store = createStore(rootReducer, enhancer);
